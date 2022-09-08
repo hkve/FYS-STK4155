3946eccb77cb5550ae8c9dbc6112c73d9569f3ff
carlmfe <carlmfe@gmail.com>
2022-09-08 19:54:36 +0200

    Implemented Data class to store/handle model data.

--- Project1/src/pipeline/pipeline.py
+++ Project1/src/pipeline/pipeline.py
@@ -7,6 +7,123 @@ from os import path
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 
 
+class Data:
+    '''
+    Class for storing and handling (y, X) data
+    '''
+    def __init__(self, y, X):
+        self.y = np.array(y)
+        self.X = np.array(X)
+        self.n_features = X.shape[-1]+1
+
+        self.scalers_ = {
+            "Standard" : self.standard_scaler_
+        }
+
+        self.unscale = lambda self : self
+
+    def __getitem__(self, key):
+        i, j = key
+        if i == 0:
+            return self.y[j]
+        elif i > 0 and i < X.shape[1]:
+            return X[j, i]
+        else:
+            raise IndexError(f"Index i out of bounds for data of with {self.n_features} features.")
+
+    def __iter__(self):
+        self.i = 0
+        yield Data(self.y[self.i], self.X[self.i])
+
+    def __next__(self):
+        self.i += 1
+
+    def __str__(self):
+        return str(np.concatenate(([self.y], [*self.X.T])).T) # messy, I know
+
+    def __add__(self, other):
+        if type(other) in (int, float, np.int64, np.float64):
+            return Data(self.y+other, self.X+other)
+        elif type(other) == np.ndarray:
+            try:
+                return Data(self.y+other[:,0], self.X+other[:,1:])
+            except IndexError:
+                return Data(self.y+other[0], self.X+other[1:])
+        elif type(other) == Data:
+            return Data(self.y+other.y, self.X+other.X)
+        else:
+            raise TypeError(f"Addition not implemented betwee Data and {type(other)}")
+    
+    def __sub__(self, other):
+        if type(other) in (int, float, np.int64, np.float64):
+            return Data(self.y-other, self.X-other)
+        elif type(other) == np.ndarray:
+            try:
+                return Data(self.y-other[:,0], self.X-other[:,1:])
+            except IndexError:
+                return Data(self.y-other[0], self.X-other[1:])
+        elif type(other) == Data:
+            return Data(self.y-other.y, self.X-other.X)
+        else:
+            raise TypeError(f"Subtraction not implemented betwee Data and {type(other)}")
+
+    def __mul__(self, other):
+        if type(other) in (int, float, np.int64, np.float64):
+            return Data(self.y*other, self.X*other)
+        elif type(other) == np.ndarray:
+            try:
+                return Data(self.y*other[:,0], self.X*other[:,1:])
+            except IndexError:
+                return Data(self.y*other[0], self.X*other[1:])
+        elif type(other) == Data:
+            return Data(self.y*other.y, self.X*other.X)
+        else:
+            raise TypeError(f"Multiplication not implemented betwee Data and {type(other)}")
+
+    def __truediv__(self, other):
+        if type(other) in (int, float, np.int64, np.float64):
+            return Data(self.y/other, self.X/other)
+        elif type(other) == np.ndarray:
+            try:
+                return Data(self.y/other[:,0], self.X/other[:,1:])
+            except IndexError:
+                return Data(self.y/other[0], self.X/other[1:])
+        elif type(other) == Data:
+            return Data(self.y/other.y, self.X/other.X)
+        else:
+            raise TypeError(f"Division not implemented betwee Data and {type(other)}")
+
+
+    def unpack(self):
+        return self.y, self.X
+
+    def train_test_split(self, ratio=2/3):
+        '''
+        Splits the data into training data and test data according to train_test-ratio
+        '''
+        size = data.shape[0]
+        np.random.shuffle(data)
+        training_data, test_data = data[:int(size*ratio)], data[int(size*ratio):]
+        return training_data, test_data
+
+    def scale(self, scheme="Standard"):
+        return self.scalers_[scheme](self)
+
+    def standard_scaler_(self, data):
+        '''
+        Scales y, *X to be N(0, 1)-distributed.
+        '''
+        data = np.concatenate(([data.y], [*data.X.T])).T
+        data_mean = np.mean(data, axis=0)
+        data_std = np.std(data, axis=0)
+        data_std = np.where(data_std != 0, data_std, 1) # sets unvaried data-columns to 0
+        data_scaled = (data - data_mean) / data_std
+
+        scaled_data = Data(data_scaled[:,0], data_scaled[:,1:])
+        scaled_data.unscale = lambda data : data*data_std + data_mean
+
+        return scaled_data
+
 
 class TrainingFacility: # working title
     def __init__(self, model, data):
@@ -18,6 +135,7 @@ class TrainingFacility: # working title
         self.data = data
 
         self.scaled_data = None
+        self.unscale_data = lambda scaled_data : None
 
         self.scalers_ = {
             "Standard" : self.standard_scaler_
@@ -43,61 +161,74 @@ class TrainingFacility: # working title
 
         NB. Schemes not implemented yet, deafults to Standard scaling of y, x to be N(1,0)-distributed.
         '''
-        y, X = self.unpack_data_(self.data)
-        ynew, Xnew = self.scalers_[scheme](y, X)
-        self.scaled_data = self.pack_data_(ynew, Xnew)
+        self.scaled_data = self.scalers_[scheme](self.data)
 
         return self
 
-    def train_test_split(self, train_test=2/3):
+    def train_test_split(self, data, train_test=2/3):
         '''
         Splits the data into training data and test data according to train_test-ratio
         '''
-        size = self.data.shape[0]
-        shuffled_data = self.data.copy(); np.random.shuffle(shuffled_data)
-        training_data, test_data = shuffled_data[:int(size*train_test)], shuffled_data[int(size*train_test):]
+        size = data.shape[0]
+        np.random.shuffle(data)
+        training_data, test_data = data[:int(size*train_test)], data[int(size*train_test):]
         return training_data, test_data
 
-    def fit_training_data(self, train_test=2/3):
+    def fit_training_data(self, scale=False, train_test=2/3):
         '''
         Fits the model to training data.
         NB. Not sure just how I want to implement this bit.
         '''
-        training_data, test_data = self.train_test_split(train_test=train_test)
+        if scale:
+            self.scale_data()
+            data = self.scaled_data
+            # print(self.scaled_data[:,:1].shape, self.scaled_data[:,2:].shape)
+            # sys.exit()
+            data = np.concatenate((self.scaled_data[:,:1], self.scaled_data[:,2:]), axis=1)
+        else:
+            data = self.data
+        training_data, self.test_data = self.train_test_split(data, train_test=train_test)
         y_training, X_training = self.unpack_data_(training_data)
         fit_model = self.model(method="INV").fit(X_training, y_training)
         return fit_model
 
-    def standard_scaler_(self, y, X):
+    def standard_scaler_(self, data):
         '''
         Scales y, *X to be N(0, 1)-distributed.
         '''
-        ynew = (y - np.mean(y)) / np.where(np.std(y)==0, 1, np.std(y))
-        Xnew = (X - np.mean(X, axis=0)) / np.where(np.std(X, axis=0)==0, 1, np.std(X, axis=0))
-        return ynew, Xnew
+        data_mean = np.mean(data, axis=0)
+        data_std = np.std(data, axis=0)
+        data_std = np.where(data_std != 0, data_std, 1) # sets unvaried data-columns to 0
+        data_scaled = (data - data_mean) / data_std
 
+        self.unscale_data = lambda scaled_data : scaled_data*data_std + data_mean
+
+        return data_scaled
 
 
 if __name__ == '__main__':
+    x = np.random.uniform(0, 1, size=10)
+    X = np.array([np.ones_like(x), x, x**2]).T
+    y = np.exp(x*x) +2*np.exp(-2*x) + 0.1*np.random.randn(x.size)
+    testdata = Data(y, X)
+    testdata_scaled = testdata.scale()
+
+
     # testing with random function I thought of
     # setting up first
     from sknotlearn.linear_model import LinearRegression
-    x = np.random.uniform(0, 1, size=100)
+    x = np.random.uniform(0, 1, size=1000)
     X = np.array([np.ones_like(x), x, x**2]).T
     beta = np.array([1, -2, 3])
     y = np.exp(x*x) + 2*np.exp(-2*x) + 0.1*np.random.randn(x.size)
+    # y = X@beta + 0.1*np.random.randn(x.size)
     data = np.array([(yi, 1, xi, xi**2) for yi, xi in zip(y,x)])
 
     # testing some functionality
     test = TrainingFacility(LinearRegression, data)
-    test.scale_data()
-    fit = test.fit_training_data()
-    print(f"MSE score : {np.mean((fit.predict(X)-y))**2}")
+    fit = test.fit_training_data(scale=True)
+    ypredict = test.unscale_data(fit.predict(test.test_data[:,2:]))
 
-    # plot result
     import matplotlib.pyplot as plt
-    plt.scatter(x, y)
-    x.sort()
-    ypredict = fit.predict(np.array([np.ones_like(x), x, x**2]).T)
-    plt.plot(x, ypredict)
-    plt.show()
+    plt.scatter(test.test_data[:,2], test.test_data[:,0])
+    plt.scatter(test.test_data[:,2], ypredict)
