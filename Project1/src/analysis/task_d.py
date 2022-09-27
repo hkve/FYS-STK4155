9cec4bec5fcfe64e2d44fe9d1b21f794cbd25f78
hkve <haakon.kvernmoen@gmail.com>
2022-09-27 15:43:37 +0200

    Starting CV

--- Project1/src/analysis/task_d.py
+++ Project1/src/analysis/task_d.py
@@ -1,16 +1,42 @@
-from random import random
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 import context
 from sknotlearn.linear_model import LinearRegression
-from sknotlearn.resampling import cross_validation
+from sknotlearn.resampling import KFold_cross_validate
 from sknotlearn.datasets import make_FrankeFunction
 from sknotlearn.data import Data
-import numpy as np
 
-def task_d(n=600, ks=np.arange(5,10+1), random_state=321):
-    mse, r2 = np.zeros_like(ks), np.zeros_like(ks)
-    
-    X, y = make_FrankeFunction(n, noise_std=0.1, random_state=random_state)
+def plot_mse(degrees, train_mse, test_mse):
+    sns.set_style("darkgrid")
+    fig, ax = plt.subplots()
 
+    ax.plot(degrees, train_mse, label="Train mse")
+    ax.plot(degrees, test_mse, label="Test mse")
+    ax.legend()
+
+    plt.show()
+
+def task_d(n=600, k=5, random_state=321):    
+    degrees = np.arange(1, 12)
+    train_mse = np.zeros_like(degrees, dtype=float)
+    test_mse = np.zeros_like(degrees, dtype=float)
+
+    D = make_FrankeFunction(n, noise_std=0.1, random_state=random_state)
+
+    for i, degree in enumerate(degrees):
+        Dp = D.polynomial(degree=degree)
+        Dp = Dp.scaled(scheme="Standard")
+
+        reg = LinearRegression()
+        resampler = KFold_cross_validate(reg, D, k=k, scoring="mse", random_state=random_state)
+
+        print(resampler["test_mse"].mean())
+        train_mse[i] = resampler["train_mse"].mean()
+        test_mse[i] = resampler["test_mse"].mean()
+        
+    plot_mse(degrees, train_mse, test_mse)
 
 if __name__ == "__main__":
-    task_d()
+    task_d(random_state=321)
