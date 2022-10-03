bf51dfbb75416713885e314c6a905fa5e04255ec
Annaaasen <65226318+Annaaasen@users.noreply.github.com>
2022-10-03 13:55:37 +0200

    Added terrain file and implementation in datasets.py

--- Project1/src/sknotlearn/datasets.py
+++ Project1/src/sknotlearn/datasets.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from imageio import imread
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from data import Data 
 
@@ -27,6 +28,7 @@ def make_FrankeFunction(n=1000, uniform=True, noise_std=0, random_state=42):
 		X, Y = np.meshgrid(x, y)
 		x = X.flatten()
 		y = Y.flatten()
+		
 
 	z = FrankeFunction(x, y) + np.random.normal(loc=0, scale=noise_std, size=n)
 
@@ -38,15 +40,17 @@ def plot_FrankeFunction(D, filename=None):
 	ax = fig.add_subplot(projection="3d")
 	surf = ax.plot_trisurf(*D.X.T, D.y, cmap=cm.viridis, linewidth=0, antialiased=False)
 
-	ax.set_zlim(-0.10, 1.40)
-	ax.zaxis.set_major_locator(LinearLocator(10))
-	ax.zaxis.set_major_formatter(FormatStrFormatter("%.02f"))
+	#TODO: Change limits
+	# ax.set_zlim(-0.10, 1.40)
+	# ax.zaxis.set_major_locator(LinearLocator(10))
+	# ax.zaxis.set_major_formatter(FormatStrFormatter("%.02f"))
 
 	ax.set_xlabel("x", fontsize=14)
 	ax.set_ylabel("y", fontsize=14)
 	ax.set_zlabel(r"$F (x,y)$", fontsize=14, rotation=90)
 	fig.colorbar(surf, shrink=0.5, aspect=5)
 	
+	#TODO: Change angle
 	polar_angle = 18 # This is "theta" is physics convention of spherical coordinates
 	azimuthal_angle = 45 # "phi"
 	ax.view_init(polar_angle, azimuthal_angle)
@@ -56,7 +60,6 @@ def plot_FrankeFunction(D, filename=None):
 
 	plt.show()
 
-
 def FrankeFunction(x,y):
 	term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
 	term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
@@ -64,6 +67,31 @@ def FrankeFunction(x,y):
 	term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
 	return term1 + term2 + term3 + term4
 
+def load_terrain(filename="SRTM_data_mot"):
+	l = np.arange(30)
+	X, Y = np.meshgrid(l,l)	
+	x = X.flatten()
+	y = Y.flatten()
+
+	z = imread(filename+".tif")[1740:1800:2, 1600:1660:2] 
+	z = z.flatten()
+	
+	return Data(z, np.c_[x,y])
+
+def plot_terrain(D):
+
+	fig = plt.figure()
+	ax = fig.gca(projection="3d")
+
+	im = ax.plot_surface(X, Y, D, cmap="viridis")
+	fig.colorbar(im)
+	plt.show()
+
+
 if __name__ == "__main__":
 	D = make_FrankeFunction(n=625, uniform=False, noise_std=0.1)
+	# plot_FrankeFunction(D)
+
+
+	D = load_terrain()
 	plot_FrankeFunction(D)
