b9cc1a7b49fc11805a92441c899d0f5a1467f734
Annaaasen <65226318+Annaaasen@users.noreply.github.com>
2022-10-26 17:46:14 +0200

    Started implementing BP. Not working atm..

--- Project2/src/sknotlearn/neuralnet.py
+++ Project2/src/sknotlearn/neuralnet.py
@@ -1,3 +1,4 @@
+from random import weibullvariate
 import numpy as np
 from autograd import grad
 from datasets import make_debugdata
@@ -11,6 +12,7 @@ class NeuralNetwork:
         self, 
         optimizer:opt.GradientDescent,
         nodes:tuple,
+        cost_func, 
         activation_hidden:str="sigmoid",
         activation_output:str="sigmoid", 
         random_state = None
@@ -32,25 +34,35 @@ class NeuralNetwork:
         self.weights = [None]*(self.n_hidden_layers + 1)
         self.biases = [None]*(self.n_hidden_layers + 1)
 
+        self.cost_func = self.cost_funcitons[cost_func]
+
         self._activation_hidden = self.activation_functions[activation_hidden]
         self._activation_output = self.activation_functions[activation_output]
 
     def _init_biases_and_weights(self) -> None:
-        if self.random_state:
-            np.random.seed(self.random_state)
-
         self.weights[0] = np.random.randn(self.n_hidden_nodes[0], self.n_features)
         self.weights[1:-1] = [np.random.randn(self.n_hidden_nodes[layer+1],self.n_hidden_nodes[layer]) for layer in range(self.n_hidden_layers-1)]
         self.weights[-1] = np.random.randn(1, self.n_hidden_nodes[-1])
-
+        
         self.biases[:-1] = [0.1*np.ones(nodes) for nodes in self.n_hidden_nodes]
         self.biases[-1] = np.array([0.1]) 
 
-    def _backprop_pass(self) -> None:
-        pass
+    def _backprop_pass(self, y, y_pred) -> None:
+        # TODO: Save deltas and update weights after the loop.
+        # TODO: Look at GD 
+
+        grad_cost = grad(lambda ypred : self.cost_func(y, ypred))
+        grad_act_out = grad(self._activation_output) 
+        delta = grad_cost * grad_act_out
+
+        for h in reversed(range(self.n_hidden_layers)):
+            grad_act_hid = grad(self._activation_hidden)
+            delta = self.weights[h+1].T @ delta * grad_act_hid
+              
+        
 
     def _forward_pass(self, x) -> None:
-        #hidden layer: 
+        #hidden layer:
         a = x
         for h in range(self.n_hidden_layers):
             z = self.weights[h] @ a + self.biases[h]
@@ -60,9 +72,6 @@ class NeuralNetwork:
         z = self.weights[-1] @ a + self.biases[-1]
         y = self._activation_output(z)
         return y
-
-    def _sigmoid(x):
-        return 1/(1+np.exp(-x))
 
     def predict(self, X:np.array) -> np.array:
         y_pred = None
@@ -76,17 +85,32 @@ class NeuralNetwork:
         #Initialize weights and biases: 
         self._init_biases_and_weights()
 
+        #A loop here!
+
         # Call forward for every datapoint: 
         n = len(self.D_train)
-        y = np.zeros(n)
-        for i, xi in enumerate(self.D_train.X):
-            y[i] = self._forward_pass(xi)
+        y, X = self.D_train.unpacked()
+        y_pred = np.zeros(n)
+        for i, xi in enumerate(X):
+            y_pred[i] = self._forward_pass(xi)
 
         # and backprop ...
-    
+        self._backprop_pass(y, y_pred)
+
+    def _sigmoid(x):
+        return 1/(1+np.exp(-x))
+
+    def _MSE(y, y_pred):
+        return np.mean((y - y_pred)**2)
+        
+    cost_funcitons = {
+        "MSE": _MSE,
+    }
+
     activation_functions = {
         "sigmoid": _sigmoid, # Add ReLU and such...    
     }
+
 
 if __name__ == "__main__":
     x, y, X = make_debugdata()
@@ -98,6 +122,9 @@ if __name__ == "__main__":
         its=100
     )
 
-    nodes = ((10, 10, 10), 1)
+    
+    nodes = ((10, 9, 10), 1)
     NN = NeuralNetwork(GD, nodes, random_state=321)
     NN.train(D)
+
+
