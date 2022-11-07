b70b157304347abdc703109959c3ddf2f14f471a
Annaaasen <65226318+Annaaasen@users.noreply.github.com>
2022-11-07 10:16:22 +0100

    Added make_figs_path

--- Project2/src/analysis/plot_utils.py
+++ Project2/src/analysis/plot_utils.py
@@ -1,6 +1,25 @@
+import pathlib as pl 
 import matplotlib.pyplot as plt
 from cycler import cycler
 import seaborn as sns
+
+def make_figs_path(filename):
+    cur_path = pl.Path(__file__)
+    root_path = cur_path
+
+    while root_path.name != "FYS-STK4155":
+        root_path = root_path.parent
+
+    figs_path = root_path / pl.Path("Project2/tex/figs")
+
+    if not figs_path.exists():
+        return None
+    if not filename.endswith(".pdf"):
+        filename += ".pdf"
+
+    figs_path /= filename
+
+    return str(figs_path)
 
 colors = [
     sns.color_palette('husl')[-3],
