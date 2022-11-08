Fix scaling in GD methods and update plots + add minmax scaling in data (not being used)

Project2/src/analysis/gradient_descent.py
@@ -8,7 +8,6 @@ import seaborn as sns
 from sys import exit
 from time import time
 import plot_utils
-from plot_utils import make_figs_path
 
 import context
 from sknotlearn.data import Data
@@ -132,7 +131,7 @@ def tune_learning_rate(
         mode="expand"
     )
     if filename:
-        plt.savefig(make_figs_path(filename))
+        plt.savefig(plot_utils.make_figs_path(filename))
     plt.show()
 
 
@@ -243,7 +242,7 @@ def tune_lambda_learning_rate(
     )
 
     if filename:
-        plt.savefig(make_figs_path(filename))
+        plt.savefig(plot_utils.make_figs_path(filename))
     plt.show()
 
     
@@ -252,8 +251,10 @@ if __name__=="__main__":
     from sknotlearn.datasets import make_FrankeFunction, load_Terrain
     # D = load_Terrain(n=600, random_state=321)
     D = make_FrankeFunction(n=600, noise_std=0.1, random_state=321)
-    D = D.polynomial(degree=5,with_intercept=False).scaled(scheme="Standard")
+    D = D.polynomial(degree=5,with_intercept=False)
     D_train, D_val = D.train_test_split(ratio=3/4, random_state=42)
+    D_train = D_train.scaled(scheme="Standard")
+    D_val = D_train.scale(D_val)
 
     # Setting some general params
     random_state = 321
@@ -289,7 +290,7 @@ if __name__=="__main__":
     #####################
     params1 = {
         "PGD_MGD_PSGD_MSGD" : {
-            "learning_rates" : np.linspace(0.001, 0.13, 101),
+            "learning_rates" : np.linspace(0.001, 0.14, 101),
             "optimizers" : (GD,mGD,SGD,mSGD),
             "optimizer_names" : (f"Plain GD",f"Momentum GD", "Plain SGD", "Momentum SGD"),
             "ylims" : (0,0.8)
@@ -360,10 +361,10 @@ if __name__=="__main__":
     ]
 
     # Plotting
-    # plot1 = plots1[1]
-    plot1 = None
-    plot2 = plots2[1]
-    # plot2 = None
+    plot1 = plots1[4]
+    # plot1 = None
+    # plot2 = plots2[0]
+    plot2 = None
 
     if plot1:
         tune_learning_rate(

Project2/src/sknotlearn/data.py
@@ -283,22 +283,61 @@ class Data:
         data_std = np.where(data_std != 0, data_std, 1) # sets unvaried data-columns to 0
         data_scaled = (data_array - data_mean) / data_std
 
+        # function to unscale scaled Data
+        unscaler = lambda data_ : data_*data_std + data_mean
         # function to replicate exact scaling done here
         def fit_scaler(data):
             out = (data - data_mean) / data_std
-            out.unscale = lambda data_ : data_*data_std + data_mean
+            out.unscale = unscaler
             return out
 
         # packing result into new Data-instance
         scaled_data = Data(
             data_scaled[:,0],
             data_scaled[:,1:],
-            unscale = lambda data : data*data_std + data_mean, # teching new Data how to unscale
+            unscale = unscaler, # teching new Data how to unscale
+            scale = fit_scaler, # teaching new Data how to scale other Data the same way.
+        )
+        return scaled_data
+
+    def min_max_(data):
+        """Scales y, *X to be distributed between -1 and 1.
+
+        Args:
+            data (Data): Data to scale
+
+        Returns:
+            Data: Scaled Data
+        """
+        # extracting data from Data-class to more versatile numpy array
+        data_array = np.c_[data.y, data.X]
+        # vectorised scaling
+        data_max = np.max(data_array, axis=0)
+        data_min = np.min(data_array, axis=0)
+        data_diff = data_max - data_min
+        data_scaled = np.where(data_diff == 0,
+                               0, (data_array-data_min) / data_diff)
+
+        # function to unscale scaled Data
+        unscaler = lambda data_ : np.where(data_diff == 0,
+                                           data_min, data_diff*data + data_min)
+        # function to replicate exact scaling done here
+        def fit_scaler(data):
+            out = (data - data_min) / data_diff
+            out.unscale = unscaler
+            return out
+
+        # packing result into new Data-instance
+        scaled_data = Data(
+            data_scaled[:,0],
+            data_scaled[:,1:],
+            unscale = unscaler, # teching new Data how to unscale
             scale = fit_scaler, # teaching new Data how to scale other Data the same way.
         )
         return scaled_data
     
     scalers_ = {
         "None" : none_scaler_,
-        "Standard" : standard_scaler_
+        "Standard" : standard_scaler_,
+        "Minmax" : min_max_,
     }