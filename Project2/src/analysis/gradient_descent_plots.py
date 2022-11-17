6770fa9fafca7c914b177393a4c2dda2d7959253
carlmfe <carlmfe@gmail.com>
2022-11-17 21:24:29 +0100

    Cleaned up and split gradient_descent.py into two files

--- Project2/src/analysis/gradient_descent_plots.py
+++ Project2/src/analysis/gradient_descent_plots.py
@@ -1,63 +1,17 @@
-'''
-This script intends to make analysis plots comparing different Gradient Descent
-methods.
-'''
+"""This file aims to make analysis plots of gradient descent methods
+used in optimising OLS and ridge cost functions of linear models"""
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sys import exit
 from time import time
-from collections.abc import Callable
-import plot_utils
 
+from gradient_descent_utils import *
+import plot_utils
 import context
 from sknotlearn.data import Data
 from sknotlearn.linear_model import OLS_gradient, ridge_gradient
-from sknotlearn.optimize import GradientDescent, SGradientDescent, isNotFinite
-
-
-def MSE(y_pred: np.ndarray, y_target: np.ndarray) -> float:
-    return np.mean((y_pred - y_target)**2)
-
-
-def OLS_solution(X: np.ndarray, y: np.ndarray) -> np.ndarray:
-    return np.linalg.pinv(X.T @ X) @ X.T @ y
-
-
-def ridge_solution(X: np.ndarray, y: np.ndarray, lmbda: float) -> np.ndarray:
-    Id = np.eye(X.shape(1))
-    return np.linalg.pinv(X.T @ X + lmbda*Id) @ X.T @ y
-
-
-def clip_exploded_gradients(val: float, lim: float, convergence: int) -> float:
-    if lim is not None:
-        limtest = val < lim
-    else:
-        limtest = True
-    if convergence and limtest:
-        return val
-    else:
-        return np.nan
-
-
-def call_optimizer(optimizer: GradientDescent,
-                   grad: Callable, x0: np.ndarray,
-                   idcs: np.ndarray) -> np.ndarray:
-    """Handles the different call signatures of GradientDescent and SGradientDescent"""
-    if isinstance(optimizer, SGradientDescent):
-        theta_opt = optimizer.call(
-            grad=grad,
-            x0=x0,
-            all_idcs=idcs
-        )
-    else:
-        theta_opt = optimizer.call(
-            grad=grad,
-            x0=x0,
-            args=(idcs,)
-        )
-    return theta_opt
-
+from sknotlearn.optimize import GradientDescent, SGradientDescent
 
 
 def tune_learning_rate(
@@ -359,41 +313,14 @@ def analytical_lmbda_plot(data_train: Data, data_val: Data, lmbdas: np.ndarray,
     plt.show()
 
 
-def do_GD_iteration(optimizer: GradientDescent, grad: Callable,
-                    data: Data, idcs: np.ndarray) -> None:
-    """Do one iteration of gradient descent 'by hand'.
-
-    Args:
-        optimizer (GradientDescent): GradientDescent instance to do iteration.
-        grad (Callable): Gradient of the relevant cost function to use.
-        data (Data): Data instance with data to fit to.
-        idcs (np.ndarray): Available indices to pass to grad.
-    """
-    # Spltting up iterations between stochastic and non-stochastic methods
-    if isinstance(optimizer, SGradientDescent):
-        batches = optimizer._make_batches(idcs)
-        for batch in batches:
-            g = grad(optimizer.x, data, batch)
-            if isNotFinite(np.abs(g)):
-                break
-            else:
-                optimizer.x = optimizer._update_rule(optimizer, optimizer.x, g)
-    else:
-        g = grad(optimizer.x, data, idcs)
-        if isNotFinite(np.abs(g)):
-            pass
-        else:
-            optimizer.x = optimizer._update_rule(optimizer, optimizer.x, g)
-
-
 def plot_by_iteration(data_train: Data, data_val: Data,
                       optimizers: tuple, optimizer_names: tuple,
                       max_iter: int, theta0: tuple,
                       random_state: int = None,
                       verbose: bool = False,
                       filename: str = None) -> None:
-    """Plot the history of the validation MSE as parameters of
-    OLS cost function are optimised using given optimizers.
+    """Plot the history by iteration of the validation MSE
+    as parameters of OLS cost function are optimised using given optimizers.
 
     Args:
         data_train (Data): Data instance with training data.
@@ -460,9 +387,6 @@ def plot_by_iteration(data_train: Data, data_val: Data,
                   f"(it {arg_best_MSE+1})")
         if len(theta0) > 1:  # Adding 95% confidence interval
             MSE_stds = MSEs.std(axis=0) / np.sqrt(len(theta0)-1)
-            # axes[1].fill_between(all_iters,
-            #                      MSE_means-2*MSE_stds, MSE_means+2*MSE_stds,
-            #                      alpha=0.3, color=color)
             axes[1].plot(all_iters, np.log10(MSE_stds),
                          color=color, lw=1, alpha=0.5)
 
@@ -494,9 +418,7 @@ if __name__ == "__main__":
     # Setting some general params
     np.random.seed(random_state)
     theta0 = [np.random.randn(D_val.n_features) for _ in range(5)]
-
     max_iter = 500
-
     batch_size = 200
 
     # All the gradient descent instances
@@ -652,7 +574,7 @@ if __name__ == "__main__":
     plot1 = ""  # dict key for params1 or empty string
     plot2 = ""  # dict key for params2 or empty string
     plot3 = 0  # True or False
-    plot4 = 1  # True or False
+    plot4 = 0  # True or False
 
     # Plotting
     if plot1:
---
+++ Project2/src/analysis/gradient_descent_utils.py
@@ -0,0 +1,78 @@
+"""This file contains utility functions used in gradient_descent_plots.py"""
+import numpy as np
+from collections.abc import Callable
+
+import context
+from sknotlearn.data import Data
+from sknotlearn.optimize import GradientDescent, SGradientDescent, isNotFinite
+
+
+def MSE(y_pred: np.ndarray, y_target: np.ndarray) -> float:
+    return np.mean((y_pred - y_target)**2)
+
+
+def OLS_solution(X: np.ndarray, y: np.ndarray) -> np.ndarray:
+    return np.linalg.pinv(X.T @ X) @ X.T @ y
+
+
+def ridge_solution(X: np.ndarray, y: np.ndarray, lmbda: float) -> np.ndarray:
+    Id = np.eye(X.shape(1))
+    return np.linalg.pinv(X.T @ X + lmbda*Id) @ X.T @ y
+
+
+def clip_exploded_gradients(val: float, lim: float, convergence: int) -> float:
+    if lim is not None:
+        limtest = val < lim
+    else:
+        limtest = True
+    if convergence and limtest:
+        return val
+    else:
+        return np.nan
+
+
+def call_optimizer(optimizer: GradientDescent,
+                   grad: Callable, x0: np.ndarray,
+                   idcs: np.ndarray) -> np.ndarray:
+    """Handles the different call signatures of
+    GradientDescent and SGradientDescent"""
+    if isinstance(optimizer, SGradientDescent):
+        theta_opt = optimizer.call(
+            grad=grad,
+            x0=x0,
+            all_idcs=idcs
+        )
+    else:
+        theta_opt = optimizer.call(
+            grad=grad,
+            x0=x0,
+            args=(idcs,)
+        )
+    return theta_opt
+
+
+def do_GD_iteration(optimizer: GradientDescent, grad: Callable,
+                    data: Data, idcs: np.ndarray) -> None:
+    """Do one iteration of gradient descent 'by hand'.
+
+    Args:
+        optimizer (GradientDescent): GradientDescent instance to do iteration.
+        grad (Callable): Gradient of the relevant cost function to use.
+        data (Data): Data instance with data to fit to.
+        idcs (np.ndarray): Available indices to pass to grad.
+    """
+    # Spltting up iterations between stochastic and non-stochastic methods
+    if isinstance(optimizer, SGradientDescent):
+        batches = optimizer._make_batches(idcs)
+        for batch in batches:
+            g = grad(optimizer.x, data, batch)
+            if isNotFinite(np.abs(g)):
+                break
+            else:
+                optimizer.x = optimizer._update_rule(optimizer, optimizer.x, g)
+    else:
+        g = grad(optimizer.x, data, idcs)
+        if isNotFinite(np.abs(g)):
+            pass
+        else:
+            optimizer.x = optimizer._update_rule(optimizer, optimizer.x, g)
