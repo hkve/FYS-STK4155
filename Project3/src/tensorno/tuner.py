887046c883624e520a1e7a759319ef795d6b861b
carlmfe <carlmfe@gmail.com>
2022-12-05 21:10:05 +0100

    Add module for tuning LWTA architecture using keras_tuner

---
+++ Project3/src/tensorno/tuner.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import keras_tuner
+
+if __name__ == "__main__":
+    from bob import LWTA_architecture_builder
+else:
+    from tensorno.bob import LWTA_architecture_builder
+
+
+def tune_LWTA_architecture(layer_type: str,
+                           train_data: tuple,
+                           val_data: tuple,
+                           isregression: bool = True,
+                           _tuner_kwargs: dict = None,
+                           _search_kwargs: dict = None) -> keras_tuner.Tuner:
+    tuner_kwargs = dict(
+        max_epochs=150,
+        hyperband_iterations=1,
+        objective="val_loss",
+        directory=layer_type,
+    )
+    if _tuner_kwargs is not None:
+        tuner_kwargs.update(_tuner_kwargs)
+
+    tuner = keras_tuner.Hyperband(
+        LWTA_architecture_builder(layer_type,
+                                  isregressor=isregression,
+                                  num_features=train_data[0].shape[-1],
+                                  num_categories=train_data[1].shape[-1]),
+        **tuner_kwargs
+    )
+    search_kwargs = dict(
+        epochs=150,
+    )
+    if _search_kwargs is not None:
+        search_kwargs.update(_search_kwargs)
+
+    tuner.search(
+        x=train_data[0],
+        y=train_data[1],
+        validation_data=val_data,
+        **search_kwargs
+    )
+
+    return tuner
