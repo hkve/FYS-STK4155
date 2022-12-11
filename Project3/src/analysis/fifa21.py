f741ce46f78928bad9b796a67b91021c4f95e360
hkve <haakon.kvernmoen@gmail.com>
2022-12-11 13:40:07 +0100

    Added linreg bias var plot (OLS, Ridge, Lasso)

--- Project3/src/analysis/fifa21.py
+++ Project3/src/analysis/fifa21.py
@@ -8,11 +8,11 @@ import fifa21_utils as utils
 
 Ridge = utils.RidgeInt
 Lasso = utils.LassoInt  
-
+LinearRegression = utils.LinearRegressionInt
 import matplotlib.ticker as mticker
 
-def LinearModel_comparison(X, y, random_state=321):
-    alpha = np.linspace(0.01, 4, 100)
+def LinearModel_comparison(X, y, filename=None, random_state=321):
+    alpha = np.logspace(-5, np.log10(5), 100)
     
     ls = {
         "mse": "solid",
@@ -23,26 +23,42 @@ def LinearModel_comparison(X, y, random_state=321):
     c = {
         Ridge: plot_utils.colors[0],
         Lasso: plot_utils.colors[1],
+        LinearRegression: plot_utils.colors[2]
     }
 
     method_params = {Ridge: {"positive": True}, Lasso: {"max_iter": 10000, "positive": True}}
     
+    mins = {}
+
     fig, ax = plt.subplots()
-    for Reg in [Ridge, Lasso]:
+    for i, Reg in enumerate([Ridge, Lasso]):
         mse, bias, var = utils.bootstrap(X_train, y_train, Reg, "alpha", alpha, method_params=method_params[Reg], random_state=rnd)
         
-        # ax.plot([], [], " ", label=Reg.__name__)
         ax.plot(alpha, mse, label="mse", ls=ls["mse"], c=c[Reg])
         ax.plot(alpha, bias, label="bias", ls=ls["bias"], c=c[Reg])
         ax.plot(alpha, var, label="var", ls=ls["var"], c=c[Reg])
+        
+        i_min = np.argmin(mse)
+        mins[Reg] = {"alpha": alpha[i_min], "mse": mse[i_min]} 
+    
+    mse, bias, var = utils.boostrap_single(X_train, y_train, method=LinearRegression, method_params={"positive": True}, random_state=rnd)
+    ax.scatter(1e-5, mse, label="mse", marker="*", color=c[LinearRegression])
+    ax.scatter(1e-5, bias, label="bias", marker="o", color=c[LinearRegression])
+    ax.scatter(1e-5, var, label="var", marker="P", color=c[LinearRegression])
 
     ax.set_yscale("log")
+    ax.set_xscale("log")
     old_lim = ax.get_ylim()
-    print(old_lim)
     new_lim = (old_lim[0], old_lim[1]*1.4)
-    print(new_lim)
-    ax.legend(ncol=2)
+    
+    x = [mins[Ridge]["alpha"], mins[Lasso]["alpha"]]
+    y = [mins[Ridge]["mse"], mins[Lasso]["mse"]]
+    ax.vlines(x=x, ymin=new_lim[0], ymax=y, ls="dashed", color="k", alpha=0.25)
+    ax.scatter(x, y, color="k", marker="x", alpha=0.25)
+    ax.legend(ncol=3)
     ax.set(xlabel=r"Regularisation $\alpha$", ylim=new_lim)
+
+    plot_utils.save(filename)
     plt.show()
 
 
@@ -52,4 +68,4 @@ if __name__ == "__main__":
     X, y = utils.get_fifa_data(n=10000, random_state=rnd)
     X_train, X_val, y_train, y_val = train_test_split(X, y, train_size=3/4, random_state=rnd)
 
-    LinearModel_comparison(X_train, y_train, random_state=rnd)
+    LinearModel_comparison(X_train, y_train, filename="BiasVar_LinearRegression", random_state=rnd)
